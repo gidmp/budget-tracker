f258e4c5b1d255e20f8020d3c560e0eed75538a1
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-07-10 19:20:41 -0700

    fixed non-responsive service worker

--- public/db.js
+++ public/db.js
@@ -1,72 +1,71 @@
-//indexDb
 
 let db;
-// create a new db request for a "budget" database.
+
+// create a new database rquest and connection
 const request = indexedDB.open("budget", 1);
 
+// on upgrade needed create a new object store called pending
 request.onupgradeneeded = function(event) {
-   // create object store called "pending" and set autoIncrement to true
-  const db = event.target.result;
-  db.createObjectStore("pending", { keyPath: "id", autoIncrement: true });
+    const db = event.target.result;
+    let objectStore = db.createObjectStore("pending", { keyPath: "id", autoIncrement: true });
 };
 
+// if request is successful, save result to db variable and check if there is internet connection. If there is run checkDatabase function
 request.onsuccess = function(event) {
-  db = event.target.result;
+    db = event.target.result;
 
-  // check if app is online before reading from db
-  if (navigator.onLine) {
-    checkDatabase();
-  }
-};
-
-//if there was an error, console log msg
-request.onerror = function(event) {
-  console.log("error! " + event.target.errorCode);
-};
-
-function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
-  const transaction = db.transaction(["pending"], "readwrite");
-
-  // access your pending object store
-  const store = transaction.objectStore("pending");
-
-  // add record to your store with add method.
-  store.add(record);
-}
-
-function checkDatabase() {
-  // open a transaction on your pending db
-  const transaction = db.transaction(["pending"], "readwrite");
-  // access your pending object store
-  const store = transaction.objectStore("pending");
-  // get all records from store and set to a variable
-  const getAll = store.getAll();
-
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pending"], "readwrite");
-
-        // access your pending object store
-        const store = transaction.objectStore("pending");
-
-        // clear all items in your store
-        store.clear();
-      });
+    if (navigator.onLine) {
+        checkDatabase();
     }
-  };
-}
+};
+
+// if there was an error with the request, log the error
+request.onerror = function(event) {
+    console.log("There was an error: ", event.target.errorCode);
+};
+
+// used in index.js if the POST request to our API fails (if there is no internet connection) -- saves recored to indexedDB database
+function saveRecord(record) {
+
+    // open a transaction and access pending object store
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+
+    // add a record to indexDB
+    pendingStore.add(record);
+};
+
+// called when internet connection is detected
+function checkDatabase() {
+
+    // open a transaction and access pending object store
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+
+    // get all records from store
+    const records = pendingStore.getAll();
+
+    // if at least one record exists in pending store, post it to our mongodb database
+    records.onsuccess = function() {
+        if (records.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(records.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                // when successfully posted to mongoDB database, open a transaction and clear records from pending object store
+                const transaction = db.transaction(["pending"], "readwrite");
+                const pendingStore = transaction.objectStore("pending");
+                pendingStore.clear();
+            })
+        }
+    }
+};
 
 // listen for app coming back online
 window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
+    "/index.js",
     "/db.js",
     "/manifest.webmanifest",
     "/styles.css",
